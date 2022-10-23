fd1c6fa4293a515619107a2aa69d0f3759c58e93
Colby <colby.r.sparks@gmail.com>
2022-10-23 15:14:40 -0700

    Simple Brake Controller Proof of Concept (#3)
    • adds functionality for translating 0-5v analog input to pwm output to control brake power module
    • creates button class with assignable callback action
    • adds button for switching on/off pwm output

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
+#include <stdint.h>
+#include <Button.h>
 #include "settings.h"
 
+/* Forward declarations */
+void toggleBrakeModule();
+
+/* Global variables */
+bool g_runBrakeModule = false;
+int64_t g_lastBrakeWrite = 0;
+uint64_t g_lastPrintTime = 0;
+
+Button button(BUTTON_PIN, INPUT_PULLUP, toggleBrakeModule);
+
 void setup() {
-  // put your setup code here, to run once:
+	Serial.begin(9600);
+	pinMode(BRAKE_CONTROL_PIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
+
+	pinMode(6, OUTPUT);
+	pinMode(A7, INPUT);
+	button.init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	button.run();
+
+	if (g_runBrakeModule) {
+		int64_t brakeVal = map(analogRead(POT_PIN), 0, 1023, 0, 255);
+
+		if (brakeVal != g_lastBrakeWrite) {
+			analogWrite(BRAKE_CONTROL_PIN, 255 - brakeVal);
+			g_lastBrakeWrite = brakeVal;
+		}
+	}
+}
+
+/* Button Callback */
+void toggleBrakeModule() {
+	g_runBrakeModule = !g_runBrakeModule;
+
+	if (g_runBrakeModule) {
+		digitalWrite(LED_BUILTIN, HIGH);
+	}
+	else {
+		digitalWrite(BRAKE_CONTROL_PIN, HIGH);
+		digitalWrite(LED_BUILTIN, LOW);
+	}
+
+	Serial.println(String("Turning brake module ") + String((g_runBrakeModule ? "On" : "Off")));
 }
--- src/settings.h
+++ src/settings.h
@@ -1,6 +1,8 @@
 #ifndef SETTINGS_H_
 #define SETTINGS_H_
 
-#include <Arduino.h>
+#define BRAKE_CONTROL_PIN 6
+#define BUTTON_PIN PIN_A0
+#define POT_PIN PIN_A7
 
 #endif
