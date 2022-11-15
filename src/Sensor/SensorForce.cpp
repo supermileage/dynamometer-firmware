c4140fc537e149d6a04e95b1205e46965e203aeb
cosparks <colby.r.sparks@gmail.com>
2022-11-14 21:20:33 -0800

    Removes Serial.println calls from SensorForce::handle

--- src/Sensor/SensorForce.cpp
+++ src/Sensor/SensorForce.cpp
@@ -1,34 +1,37 @@
+#include "settings.h"
 #include "SensorForce.h"
+
 #define READ_INTERVAL 1000
+
 #define MIN_READ_VALUE 103
 #define MAX_READ_VALUE 977
 
-        SensorForce::SensorForce(pin_size_t pinNumber, PinMode mode) {
-            _pinNumber = pinNumber;
-            _pinMode = mode;
-        }
+SensorForce::SensorForce(pin_size_t pinNumber, PinMode mode) {
+    _pinNumber = pinNumber;
+    _pinMode = mode;
+}
 
-        SensorForce::~SensorForce() { }
+SensorForce::~SensorForce() { }
+
+String SensorForce::getHumanName() {
+    Serial.println(String("Force Sensor"));
+}
+
+void SensorForce::begin() {
+    pinMode(_pinNumber, _pinMode);
+}
+
+void SensorForce::handle() {
+    if(millis() > _lastReadTime + READ_INTERVAL) {
+        _lastReadTime = millis();
         
-        String SensorForce::getHumanName() {
-            Serial.println(String("Force Sensor"));
-        }
+        int int_force = analogRead(_pinNumber);
+        _force = ((float)(int_force < MIN_READ_VALUE ?
+            0 : int_force - MIN_READ_VALUE) / (MAX_READ_VALUE - MIN_READ_VALUE)) * 25;
+    }
+}
 
-        void SensorForce::begin() {
-            pinMode(_pinNumber, _pinMode);
-        }
-
-        void SensorForce::handle() {
-            if(millis() > _lastReadTime + READ_INTERVAL) {
-                _lastReadTime = millis();
-                int int_force = analogRead(_pinNumber);
-                Serial.println(String(int_force));
-                _force = ((float)(int_force < MIN_READ_VALUE ? 0 : int_force - MIN_READ_VALUE) / (MAX_READ_VALUE - MIN_READ_VALUE)) * 25;
-                Serial.println(String("Force Reading: " ) + String(_force) + String(" lbf"));
-            }
-        }
-
-        float SensorForce::getForce() {
-            return _force;
-        }
+float SensorForce::getForce() {
+    return _force;
+}
 
