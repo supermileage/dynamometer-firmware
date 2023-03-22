991893685459412ebb5c47e19d5f14fec6bb9b52
cosparks <colby.r.sparks@gmail.com>
2023-03-21 19:53:47 -0700

    added set/get for readInterval -- fixed implicit cast of double -> float and compiler error

--- src/Sensor/SensorOptical.h
+++ src/Sensor/SensorOptical.h
@@ -5,24 +5,29 @@
 #include "Sensor.h"
 #include "PIO/pio_counter_program.pio.h"
 
+#define SENSOR_OPTICAL_DEFAULT_READ_INTERVAL 1000
+
 class SensorOptical: public Sensor {
     public:
         static const uint16_t NumApertures;
         static const float VelocityFactor;
-        SensorOptical(PIO pio, uint stateMachine, pin_size_t pinNumber);
+
+        SensorOptical(PIO pio, uint stateMachine, uint32_t readInterval = SENSOR_OPTICAL_DEFAULT_READ_INTERVAL);
         ~SensorOptical();
         String getHumanName() override;
         void begin();
         void handle();
-        float getAngularVelocity();
-        float getLinearVelocity();
+        double getAngularVelocity();
+        double getLinearVelocity();
+        void setReadInterval(uint32_t interval);
+        uint32_t getReadInterval();
 
     private:
-        uint8_t _pinNumber;
         PIO _pio;
         uint _stateMachine; 
         uint32_t _lastUpdateTime = 0;
         int32_t _lastUpdateCount = 0;
+        uint32_t _readInterval = 0;
         double _angularVelocity = 0;
 
         // for debugging purposes
