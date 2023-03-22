26147c80fde3c0be3a3fcbb7bc478cc79e8c6560
Alex Ge <48578176+LaPommeCosmique@users.noreply.github.com>
2023-03-21 20:03:28 -0700

    Calculates angular velocity at higher resolution (#25)
    * Adds functionality to calculate linaer velocity of vehicle
    * modifies display to show vehicle speed instead of angular velocity
    * adds units to display
    * adds member field for angular velocity and moved velocity calculation to handle() method
    * added set/get for readInterval -- fixed implicit cast of double -> float and compiler error
    ---------
    
    Co-authored-by: Alex Ge <alex.ge@lmi3d.net>
    Co-authored-by: cosparks <colby.r.sparks@gmail.com>

--- src/Sensor/SensorOptical.h
+++ src/Sensor/SensorOptical.h
@@ -5,22 +5,32 @@
 #include "Sensor.h"
 #include "PIO/pio_counter_program.pio.h"
 
+#define SENSOR_OPTICAL_DEFAULT_READ_INTERVAL 1000
+
 class SensorOptical: public Sensor {
     public:
         static const uint16_t NumApertures;
-        SensorOptical(PIO pio, uint stateMachine, pin_size_t pinNumber);
+        static const float VelocityFactor;
+
+        SensorOptical(PIO pio, uint stateMachine, uint32_t readInterval = SENSOR_OPTICAL_DEFAULT_READ_INTERVAL);
         ~SensorOptical();
         String getHumanName() override;
         void begin();
         void handle();
-        float getAngularVelocity();
+        double getAngularVelocity();
+        double getLinearVelocity();
+        void setReadInterval(uint32_t interval);
+        uint32_t getReadInterval();
 
     private:
-        uint8_t _pinNumber;
         PIO _pio;
         uint _stateMachine; 
-        uint32_t _lastComputeTime = 0;
-        int32_t _lastCount = 0;
+        uint32_t _lastUpdateTime = 0;
+        int32_t _lastUpdateCount = 0;
+        uint32_t _readInterval = 0;
+        double _angularVelocity = 0;
+
+        // for debugging purposes
         int32_t _lastDisplayCount = 0;
 };
 
