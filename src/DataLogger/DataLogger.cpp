d242c5d8a9ed99c37631995254af74f50cbc18e0
underscoreHasan <hasan.nr49@gmail.com>
2023-03-24 14:23:50 -0700

    Completes DataLogger::loadCSV adds debugging print statements TODO: testing through main.cpp

--- src/DataLogger/DataLogger.cpp
+++ src/DataLogger/DataLogger.cpp
@@ -7,8 +7,16 @@ using namespace std;
 
 DataLogger::DataLogger(int pinNumber)
 {
-    SD.begin(pinNumber, SPI1);
-};
+    if (SD.begin(pinNumber, SPI1))
+    {
+        DEBUG_SERIAL_LN("SD card is present & ready");
+    }
+    else
+    {
+        DEBUG_SERIAL_LN("SD card missing or failed");
+        while (1); // wait here forever
+    }
+}
 
 DataLogger::~DataLogger(){};
 
@@ -16,35 +24,70 @@ bool DataLogger::createCSV(String path, vector<String> header)
 {
     _curFile = SD.open(path, FILE_WRITE);
     _numColumns = sizeof(header);
-
+    
     if (_curFile)
     {
+        DEBUG_SERIAL_LN("Writing header to file.");
         setHeader(header);
         return true;
     }
     else
     {
+        DEBUG_SERIAL_LN("Creating file failed.");
         return false;
     }
 }
 
 bool DataLogger::loadCSV(String path)
 {
-    // TODO: add logic to update _numColumns once new file is loaded.
-    _curFile = SD.open(path, FILE_WRITE);
+    int i = 0;
+    File tempFile;
 
-    return _curFile;
+    if (!SD.exists(path))
+    {
+        DEBUG_SERIAL_LN(path + " doesn't exist.");
+        return false;
+    }
+
+    DEBUG_SERIAL_LN("Loading " + path);
+    tempFile = SD.open(path);
+
+    while (tempFile.available())
+    {
+        char readChar = tempFile.read(); // char to hold a character that has been read
+
+        if (readChar == ',')
+        {
+            i++;
+            DEBUG_SERIAL_LN("Counted " + (String) i + " columns.");
+        }
+
+        if (readChar == '\n')
+        {
+            _curFile = tempFile;
+            _numColumns = i;
+            DEBUG_SERIAL_LN("Counted " + (String) i + " columns and reached end of row.");
+            return true; // reached the end of a row, so stop counting columns and return true
+        }
+    }
+
+    _curFile = tempFile;
+    _numColumns = i;
+    DEBUG_SERIAL_LN("Counted no " + (String) i + " columns. Blank file.");
+    return true; // if while loop is never entered, there are no bytes in the file. Loading succeeds but file is blank
 }
 
 bool DataLogger::writeToLog(vector<float> values)
 {
     if (_curFile && _numColumns == sizeof(values))
     {
+        DEBUG_SERIAL_LN("Writing " + (String) sizeof(values) + " to log.");
         writeRow(values);
         return true;
     }
     else
     {
+        DEBUG_SERIAL_LN("Failed to write to log.");
         return false;
     }
 }
@@ -54,10 +97,12 @@ bool DataLogger::close()
     if (_curFile)
     {
         _curFile.close();
+        DEBUG_SERIAL_LN("Closed loaded file.");
         return true;
     }
     else
     {
+        DEBUG_SERIAL_LN("Failed to close loaded file.");
         return false;
     }
 }
@@ -85,7 +130,9 @@ void DataLogger::setHeader(vector<String> header)
         n++;
     }
 
+    DEBUG_SERIAL_LN("Header is: " + formattedHeader);
     _curFile.println(formattedHeader);
+    DEBUG_SERIAL_LN("Wrote header to file.");
 }
 
 void DataLogger::writeRow(vector<float> values)
@@ -107,4 +154,5 @@ void DataLogger::writeRow(vector<float> values)
     }
 
     _curFile.println(formattedRow);
+    DEBUG_SERIAL_LN("Wrote " + formattedRow + " to log.");
 }
