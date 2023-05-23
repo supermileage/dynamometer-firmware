d86f6ff58ff4c3eb45c63424d0d719fc85d67194
cosparks <colby.r.sparks@gmail.com>
2023-05-23 10:36:52 -0700

    Adds documentation for hardware input classes

--- src/System/HardwareDemuxer.h
+++ src/System/HardwareDemuxer.h
@@ -3,6 +3,9 @@
 
 #include "Arduino.h"
 
+/**
+ * @brief provides means of controlling select pins of dyno demux ic
+*/
 class HardwareDemuxer {
     public:
         HardwareDemuxer(pin_size_t select0, pin_size_t select1, pin_size_t output) :
@@ -16,9 +19,14 @@ class HardwareDemuxer {
             pinMode(_output, INPUT_PULLDOWN);
         }
 
+        /**
+         * @brief sets select pins to id
+         * 
+         * @note id must be unsigned 2-bit value: 0b00, 0b01, 0b10, or 0b11
+        */
         inline void setSelect(uint8_t selectId) {
-            digitalWrite(_select0, static_cast<PinMode>(selectId & 0x1));
-            digitalWrite(_select1, static_cast<PinMode>(selectId & 0x2));
+            digitalWrite(_select0, static_cast<PinStatus>(selectId & 0x1));
+            digitalWrite(_select1, static_cast<PinStatus>(selectId & 0x2));
         }
 
         inline pin_size_t getOutputPin() {
--- src/System/HardwarePotentiometer.h
+++ src/System/HardwarePotentiometer.h
@@ -8,6 +8,12 @@
 #define ALPHA 0.125f
 #define POTENTIOMETER_MAX 1016
 
+/**
+ * @brief interprets analog pin as potentiometer value
+ * 
+ * @note computes exponential moving average to smooth over sampling variance: 
+ * pico's ADC can vary +-2 units per sample
+*/
 class HardwarePotentiometer : public HardwareInput {
     public:
         HardwarePotentiometer(pin_size_t pin) : _pin(pin) { }
@@ -22,12 +28,14 @@ class HardwarePotentiometer : public HardwareInput {
         void run() override {
             int32_t readVal = analogRead(_pin);
 
-            // compute exponential moving average ()
+            // exponential moving average
             float accumulator = static_cast<float>(_lastReadVal) * (1 - ALPHA);
             readVal = static_cast<int32_t>(accumulator + static_cast<float>(readVal) * ALPHA);
 
             if (readVal != _lastReadVal) {
                 input_data_t data = POTENTIOMETER_MAX - readVal;
+                
+                // data value shouldn't be less than 0 but if it is, set it to zero
                 (_action)(data < 0 ? 0 : data);
                 _lastReadVal = readVal;
             }
