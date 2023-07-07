604bfaee66fc9d2498b5f0ad0405d135cb773d94
cosparks <colby.r.sparks@gmail.com>
2023-07-07 14:39:00 -0700

    Changes name of edit string ID macro

--- src/application/application.h
+++ src/application/application.h
@@ -72,7 +72,7 @@ namespace application {
     extern std::unordered_map<uint8_t, String> GlobalSettings;
 
     /* config ids */
-    #define CONFIG_ID_EDIT_STRING_ID            0
+    #define CONFIG_ID_EDIT_STRING_GLOBAL_ID     0
     #define CONFIG_ID_DEFAULT_OUTPUT_FILENAME   1
     #define CONFIG_ID_DEFAULT_INPUT_FILENAME    2
     #define CONFIG_ID_DIALOG_START_STRING       3
