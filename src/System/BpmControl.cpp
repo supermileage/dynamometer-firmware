446597912459ba97b2e9aa7088ed45285aff9cec
Kevin Ding <kevin.zhiyuan.ding@gmail.com>
2024-07-14 18:00:16 -0700

    Fix merge conflict, include assertion to control signal input, and change code style to fit standard

--- src/System/BpmControl.cpp
+++ src/System/BpmControl.cpp
@@ -1,33 +1,37 @@
 #include "HardwareInput.h"
 #include "BpmControl.h"
+#include "ErrorLogger.h"
 
 #define ANALOG_MAX 1023
 #define ANALOG_FREQ 20000
 
-BpmControl::BpmControl (pin_size_t pinNum){
+BpmControl::BpmControl(pin_size_t pinNum){
     _pinNum = pinNum;
 }
 
-void BpmControl::init (){
+void BpmControl::init(){
     pinMode(_pinNum, OUTPUT);
     analogWriteRange(ANALOG_MAX);
     analogWriteFreq(ANALOG_FREQ);
 };
 
-void BpmControl::setControlSignal (int32_t controlSignal){ 
+void BpmControl::setControlSignal(int32_t controlSignal){ 
     if (_active == true){
         if (controlSignal >= 0 && controlSignal <= 1023){
-            analogWrite (_pinNum, controlSignal);
+            analogWrite(_pinNum, controlSignal);
+        }
+        else {
+            dyno_assert(controlSignal >= 0 && controlSignal <= 1023);
         }
     }
     else {
-        analogWrite (_pinNum, 0);
+        analogWrite(_pinNum, 0);
     }
 };
 
-void BpmControl::setActive (bool active){
+void BpmControl::setActive(bool active){
     _active = active;
     if (active == false) {
-        analogWrite (_pinNum, 0);
+        analogWrite(_pinNum, 0);
     }
 };
